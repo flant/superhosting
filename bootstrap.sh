0c7adec762543693824933e8a01656f8715c7d0b
Timofey.Kirillov <timofey.kirillov@flant.ru>
2016-02-20 18:16:00 +0300

    Vagrant++

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env /bin/bash
 
+echo $(cat /usr/share/zoneinfo/Europe/Moscow | bzip2 -9 | base64 -w0) | base64 -d | bunzip2 > /etc/localtime
+echo Europe/Moscow > /etc/timezone
+
 apt-get update
 apt-get install -y build-essential libpam0g-dev apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -13,12 +16,13 @@ cd pam_docker
 make
 make install-ubuntu-14.04
 
+curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
 curl -sSL https://get.rvm.io | sudo bash -s stable
-source /etc/profile
+source /etc/profile.d/rvm.sh
 
-rvm install 2.2.1 --verify-downloads 1 --disable-binary --quiet-curl
-rvm alias create default 2.2.1
-gem install bundler
+rvm install 2.2.1 --quiet-curl
+rvm --default use 2.2.1
 
 cd /vagrant
+gem install bundler
 bundle install
