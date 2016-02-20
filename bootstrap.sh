b926af203d30eefc6b1f12c17b648ccc09672d36
Timofey.Kirillov <timofey.kirillov@flant.ru>
2016-02-20 20:35:55 +0300

    Vagrant++

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env /bin/bash
 
-echo $(cat /usr/share/zoneinfo/Europe/Moscow | bzip2 -9 | base64 -w0) | base64 -d | bunzip2 > /etc/localtime
-echo Europe/Moscow > /etc/timezone
-
 apt-get update
 apt-get install -y build-essential libpam0g-dev apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install -y docker-engine
+
+gpasswd -a vagrant docker
+service docker restart
 
 cd /tmp
 git clone https://github.com/flant/pam_docker.git
