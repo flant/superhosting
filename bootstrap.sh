ef75ec5de09ff5ce3962419bec4c9be46e9809d4
Timofey Kirillov <distorhead@gmail.com>
2016-03-21 18:31:36 +0300

    Install pam-docker from packagecloud flant/pam_docker.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,12 @@ apt-get update
 apt-get install -y build-essential libpam0g-dev apt-transport-https ca-certificates tree
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo 'deb https://apt.dockerproject.org/repo ubuntu-trusty main' > /etc/apt/sources.list.d/docker.list
+curl -s https://packagecloud.io/install/repositories/flant/pam_docker/script.deb.sh | sudo bash
 apt-get update
-apt-get install -y docker-engine
+apt-get install -y docker-engine pam-docker
 
 gpasswd -a vagrant docker
 service docker restart
-
-cd /tmp
-git clone https://github.com/flant/pam_docker.git
-cd pam_docker
-make
-make install-ubuntu-14.04
 
 curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 curl -sSL https://get.rvm.io | sudo bash -s stable
