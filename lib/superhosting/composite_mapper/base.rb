1fab922fd53172e20b5e34df89ce2a14e8f2c134
Alexey Igrychev <pes.sutuliy@gmail.com>
2016-04-04 10:27:23 +0300

    modified CompositeMapper

--- lib/superhosting/composite_mapper/base.rb
+++ lib/superhosting/composite_mapper/base.rb
@@ -1,14 +1,16 @@
 module Superhosting
   module CompositeMapper
-    class Base < PathMapper::DirNode
+    class Base
       attr_accessor :etc, :lib, :web
 
       def initialize(etc_mapper:, lib_mapper:, web_mapper:)
-        super(etc_mapper.path)
-        self.inheritance = etc_mapper.inheritance
-        self.etc = self
+        self.etc = etc_mapper
         self.lib = lib_mapper
         self.web = web_mapper
+      end
+
+      def method_missing(m, *args, &block)
+        self.etc.send(m, *args, &block)
       end
     end
   end
