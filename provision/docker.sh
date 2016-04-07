b447ba35cfa9d96d73874fd7e3672aa262eaacaf
Alexey Igrychev <alexey.igrychev@flant.ru>
2016-04-08 02:09:14 +0300

    +

--- provision/docker.sh
+++ provision/docker.sh
@@ -4,14 +4,13 @@ docker build -t sx-base -f dockerfile/base .
 docker build -t sx-almost-base -f dockerfile/almost_base .
 docker build -t sx-mux -f dockerfile/mux .
 
-docker tag sx-base:latest superhosting/fcgi
-docker tag sx-base:latest superhosting/test
-docker tag sx-almost-base:latest superhosting/almostbase
-docker tag sx-almost-base:latest superhosting/cphp:5.3
-docker tag sx-almost-base:latest superhosting/cphp:5.5
-docker tag sx-almost-base:latest superhosting/cphp:5.6
-docker tag sx-mux:latest superhosting/php:5.3
-docker tag sx-mux:latest superhosting/php:5.5
-docker tag sx-mux:latest superhosting/php:5.6
-docker tag sx-mux:latest superhosting/mux
-docker tag sx-mux:latest superhosting/mux
+docker tag sx-base superhosting/fcgi
+docker tag sx-base superhosting/test
+docker tag sx-almost-base superhosting/almostbase
+docker tag sx-almost-base superhosting/cphp:5.3
+docker tag sx-almost-base superhosting/cphp:5.5
+docker tag sx-almost-base superhosting/cphp:5.6
+docker tag sx-mux superhosting/php:5.3
+docker tag sx-mux superhosting/php:5.5
+docker tag sx-mux superhosting/php:5.6
+docker tag sx-mux superhosting/mux
