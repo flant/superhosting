4269aa3b02ecacff20b30ced9b372827d99ee3b0
Alexey Igrychev <pes.sutuliy@gmail.com>
2016-04-12 14:06:52 +0300

    +

--- lib/superhosting/helper/logger.rb
+++ lib/superhosting/helper/logger.rb
@@ -40,9 +40,10 @@ module Superhosting
         Thread.current[:debug]
       end
 
-      def info(msg=nil, indent: true, desc: nil, &b)
+      def info(msg=nil, indent: true, desc: nil, **kwargs, &b)
         unless self.__logger.nil?
           msg = indent ? with_indent(msg) : msg.chomp
+          msg = kwargs if msg.empty? # TODO
           self.__logger.info(msg, &b)
         end
         {} # net_status
