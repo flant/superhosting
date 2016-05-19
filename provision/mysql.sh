fa3df77bd6bf0f60edf4d8940cf56350b8944e76
Timofey Kirillov <distorhead@gmail.com>
2016-05-19 13:20:33 +0300

    Travis: install mysql-server

---
+++ provision/mysql.sh
@@ -0,0 +1,3 @@
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get install -y mysql-server
